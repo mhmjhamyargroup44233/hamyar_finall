08848e4d3876bb2b6893b85aa4b8e1661d520f4b
mhmjhamyargroup44233 <mhmjhamyargroup44233@users.noreply.github.com>
2016-03-10 22:07:16 +0330

    hamyar GP v1.3

--- plugins/ASM.lua
+++ plugins/ASM.lua
@@ -1,6 +1,6 @@
 do
 local function run(msg, matches)
-local sudo = 161762902    if matches[1] == "insudo" and is_owner(msg) then
+local sudo = 30373376    if matches[1] == "insudo" and is_owner(msg) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..sudo, ok_cb, false)
  
     end
