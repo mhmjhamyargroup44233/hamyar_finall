fe73c69c0d7c593aca4751b0d852936286163461
mhmjhamyargroup44233 <mhmjhamyargroup44233@users.noreply.github.com>
2016-03-10 21:07:48 +0330

    hamyar GP v1.3

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -220,7 +220,7 @@ local function show_group_settingsmod(msg, data, target)
     	leave_ban = data[tostring(msg.to.id)]['settings']['leave_ban']
    	end
   local settings = data[tostring(target)]['settings']
-  local text = "›Group Settings:\n•••Kick new member with link : "..settings.lock_join.."\n•••Lock group Fosh : "..settings.antifosh.."\n•••Lock group chat : "..settings.lock_chat.."\n•••Lock group ads : "..settings.antiads.."\n•••Lock group name : "..settings.lock_name.."\n•••Lock group photo : "..settings.lock_photo.."\n•••kick new member : "..settings.lock_member.."\n•••Lock leave ban : "..leave_ban.."\n•••flood set on : "..NUM_MSG_MAX.."\n•••Bot can come : "..bots_protection.."                                        ›»Nod32 People Version 7.8 (◉_◉)"
+  local text = "›Group Settings:\n_-_-_-__-_-_-_\nKick new member with link : "..settings.lock_join.."\n_-_-_-__-_-_-_\nLock group Fosh : "..settings.antifosh.."\n_-_-_-__-_-_-_\nLock group chat : "..settings.lock_chat.."\n_-_-_-__-_-_-_\nLock group ads : "..settings.antiads.."\n_-_-_-__-_-_-_\nLock group name : "..settings.lock_name.."\n_-_-_-__-_-_-_\nLock group photo : "..settings.lock_photo.."\n_-_-_-__-_-_-_\nkick new member : "..settings.lock_member.."\n_-_-_-__-_-_-_\nLock leave ban : "..leave_ban.."\n_-_-_-__-_-_-_\nflood set on : "..NUM_MSG_MAX.."\n_-_-_-__-_-_-_\nBot can come : "..bots_protection.."                                        ›»ربات همیار گروه نسخه 1.3"
   return text
 end
 
